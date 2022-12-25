fd29a6bc3517524d43d8c15d378f41577105db1f
2wr <taolaolgc@gmail.com>
2022-12-25 22:10:18 +0700

    test code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+mod terminal;
+use terminal::Terminal;
+use terminal::frame;
+use terminal::Size;
+use crossterm::style::Color;
+
 fn main() {
-    println!("Hello, world!");
+    let term = Terminal::init().unwrap();
+    term.display(&frame {
+        size: Size { width: 3, height: 3 },
+        content: vec![Color::Red, Color::Black, Color::Blue,
+                      Color::Green, Color::Yellow, Color::White,
+                      Color::Grey, Color::Magenta, Color::DarkGreen,
+        ]
+    }).unwrap();
 }
